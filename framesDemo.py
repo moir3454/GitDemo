be935168b8b9bc72fd206f5627bb5a270b9e3909
elena <moir3454.com>
2025-03-12 18:18:41 -0400

    this is commit from ATZ

--- framesDemo.py
+++ framesDemo.py
@@ -10,6 +10,7 @@ driver.get("https://the-internet.herokuapp.com/iframe")
 driver.switch_to.frame("mce_0_ifr")
 driver.find_element(By.ID,"tinymce").clear()
 driver.find_element(By.ID,"tinymce").send_keys("I am able to automate frames")
+driver.find_element(By.ID,"tinymce").send_keys("GIT TEST")
 driver.switch_to.default_content()
 print(driver.find_element(By.CSS_SELECTOR,"h3").text)
 
